21076d1fdc25f3f8d9ede4576dcb202abe9f1643
Bright11 <chikanwazuo@gmail.com>
2023-11-12 07:03:49 +0000

    logged out

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -45,6 +45,9 @@ class AuthController extends Controller
     }
 
     public function logout(){
-        return response()->json("This is my logout method");
+        Auth::user()->currentAccessToken()->delete();
+        return $this->success([
+            'message'=>'You have successfully logged out and your token has been deleted'
+        ]);
     }
 }
